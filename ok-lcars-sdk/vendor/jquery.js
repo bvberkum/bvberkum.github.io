9a76a370f44506899db293be34950a9aca28f627
B. van Berkum <dev@dotmpe.com>
2019-04-24 17:48:12 +0200

    New OK! LCARS files

---
+++ ok-lcars-sdk/vendor/jquery.js
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from notus.lan:4507/vendor/jquery.js by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 24 Apr 2019 15:47:41 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from notus.lan:4507/vendor/jquery.js by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 24 Apr 2019 15:47:41 GMT -->
+</HTML>
