2b0a9f532163d820a72f86847325681d7624a14e
B. van Berkum <dev@dotmpe.com>
2021-03-16 23:31:39 +0100

    OK-LCARS-SDK 0.0.4-dev

---
+++ ok-lcars-sdk/vendor/bootstrap-table.js
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from t460s:7011/vendor/bootstrap-table.js by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Mar 2021 22:44:25 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from t460s:7011/vendor/bootstrap-table.js by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Mar 2021 22:44:25 GMT -->
+</HTML>
